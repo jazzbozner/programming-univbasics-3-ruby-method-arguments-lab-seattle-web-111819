5702c77323e93af5a10ee307229dff0a89d44336
Jazz Bozner <jazzbozner@gmail.com>
2019-11-11 20:26:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,7 +10,7 @@ def introduction_with_language(name, language = 'Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name = 'Edwin', language = 'Ruby')
+def introduction_with_language_optional(name = 'Edwin', language)
   puts "Hi, my name is #{name} and I am leanring to program in #{language}."
 end
 
